4d66195efd0c268bc669e6b0f99f7fcc38de0bac
aayushxadhikari <aayushxadhikari@gmail.com>
2025-01-11 21:45:27 +0545

    adding the validators

--- lib/bloc/login_bloc.dart
+++ lib/bloc/login_bloc.dart
@@ -1,21 +1,26 @@
 import 'dart:async';
 
 import 'package:authentication/bloc/validators.dart';
+import 'package:rxdart/rxdart.dart';
 import 'package:rxdart/subjects.dart';
 
-class LoginBloc with Validators{
+class LoginBloc with Validators {
   final _loginEmail = BehaviorSubject<String>();
   final _loginPassword = BehaviorSubject<String>();
 
   // Getters
   Stream<String> get loginEmail => _loginEmail.stream.transform(emailHandler);
-  Stream<String> get loginPassword => _loginPassword.stream.transform(passwordHandler);
+  Stream<String> get loginPassword =>
+      _loginPassword.stream.transform(passwordHandler);
+
+  Stream<bool> get isValid =>
+    Rx.combineLatest2(loginEmail, loginPassword, (email, password) {
+      return email.isNotEmpty && password.isNotEmpty;
+    });
 
   // Setters
   Function(String) get changeLoginEmail => _loginEmail.sink.add;
   Function(String) get changeLoginPassword => _loginPassword.sink.add;
-
-  
 
   void dispose() {
     _loginEmail.close();
--- lib/bloc/register_bloc.dart
+++ lib/bloc/register_bloc.dart
@@ -1,25 +1,51 @@
 import 'package:authentication/bloc/validators.dart';
+import 'package:rxdart/rxdart.dart';
 import 'package:rxdart/subjects.dart';
 
-class RegisterBloc with Validators{
+class RegisterBloc with Validators {
   final _registerFullName = BehaviorSubject<String>();
   final _registerEmail = BehaviorSubject<String>();
   final _registerPassword = BehaviorSubject<String>();
   final _registerConfirmPassword = BehaviorSubject<String>();
 
-
   // Getters
-  Stream<String> get registerFullName => _registerFullName.stream.transform(nameHandler);
-  Stream<String> get registerEmail => _registerEmail.stream.transform(emailHandler);
-  Stream<String> get registerPassword => _registerPassword.stream.transform(passwordHandler);
-  Stream<String> get registerConfirmPassword => _registerConfirmPassword.stream.transform(passwordHandler);
+  Stream<String> get registerFullName =>
+      _registerFullName.stream.transform(nameHandler);
+  Stream<String> get registerEmail =>
+      _registerEmail.stream.transform(emailHandler);
+  Stream<String> get registerPassword =>
+      _registerPassword.stream.transform(passwordHandler);
+  Stream<String> get registerConfirmPassword =>
+      _registerConfirmPassword.stream.transform(passwordHandler);
+
+  Stream<bool> get isValid => Rx.combineLatest4(
+        registerFullName,
+        registerEmail,
+        registerPassword,
+        registerConfirmPassword,
+        (fullName, email, password, confirmPassword) {
+          return fullName.isNotEmpty &&
+              email.isNotEmpty &&
+              email.contains('@') &&
+              password.isNotEmpty &&
+              password.length >= 8;
+        },
+      );
+
+  Stream<bool> get isPasswordMatched =>
+      Rx.combineLatest2(registerPassword, registerConfirmPassword,
+          (password, confirmPassword) {
+        return password == confirmPassword;
+      }
+    );
 
   // Setters
 
   Function(String) get changeFullName => _registerFullName.sink.add;
   Function(String) get changeRegisterEmail => _registerEmail.sink.add;
   Function(String) get changeRegisterPassword => _registerPassword.sink.add;
-  Function(String) get changeConfirmPassword => _registerConfirmPassword.sink.add;
+  Function(String) get changeConfirmPassword =>
+      _registerConfirmPassword.sink.add;
 
   void dispose() {
     _registerFullName.close();
